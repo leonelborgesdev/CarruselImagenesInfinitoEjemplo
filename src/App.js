98e5b5b60d276647ca87c5b42db9391f363a2844
leonelborgesdev <leonel03t@gmail.com>
2024-01-22 11:18:41 -0400

    app carrusel

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App">holi</div>;
-}
+const Carousel = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-export default App;
+  const images = [
+    "imagen1.jpg",
+    "imagen2.jpg",
+    "imagen3.jpg",
+    // Agrega más imágenes según sea necesario
+  ];
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 3000); // Cambia el tiempo de transición según tus necesidades
+
+    return () => clearInterval(intervalId);
+  }, [currentIndex, images.length]);
+
+  return (
+    <div className="carousel-container">
+      {images.map((image, index) => (
+        <div
+          key={index}
+          className={`carousel-slide ${index === currentIndex ? "active" : ""}`}
+        >
+          <img src={image} alt={`Imagen ${index + 1}`} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Carousel;
